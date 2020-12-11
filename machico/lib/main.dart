9c75ce561423e3995c2cbbff7e30ad05ab56baf1
yamanetaisei <swiftmantaisei@gmail.com>
2020-12-11 19:29:22 +0900

    コメントを入れた

--- machico/lib/main.dart
+++ machico/lib/main.dart
@@ -12,6 +12,8 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
 
+
+        //差分
         primarySwatch: Colors.blue,
 
         visualDensity: VisualDensity.adaptivePlatformDensity,
