65c922d91fa91782e8f7108d2717ad675c332549
KanaiYuta <b1947571@planet.kanazawa-it.ac.jp>
2020-12-16 17:57:19 +0900

    2画面目の作成と仮のヘッダー追加

---
+++ machico/lib/store_details.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class StoreDetails extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+      ),
+    );
+    throw UnimplementedError();
+  }
+
+}
