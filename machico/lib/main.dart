8120a549e198b8ec58f816514f1258014c5f11f1
yamanetaisei <swiftmantaisei@gmail.com>
2020-12-18 18:18:19 +0900

    不要なコメントを削除

--- machico/lib/main.dart
+++ machico/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
 
 
-        //差分
+        
         primarySwatch: Colors.blue,
 
         visualDensity: VisualDensity.adaptivePlatformDensity,
