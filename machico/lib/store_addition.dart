ccbcab40b394af0944dcc9a3baf22a4a52d384f5
KanaiYuta <b1947571@planet.kanazawa-it.ac.jp>
2021-01-15 18:00:04 +0900

    大雑把に各要素を追加

--- machico/lib/store_addition.dart
+++ machico/lib/store_addition.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:machico/hex_color.dart';
+import 'package:machico/main.dart';
 
 class StoreAddition extends StatelessWidget{
   @override
@@ -14,6 +15,40 @@ class StoreAddition extends StatelessWidget{
           margin: EdgeInsets.all(10.0),
           color: HexColor('#F6FFF7'), // HexColorで指定
           height: double.infinity,
+          child: Column(
+            children: <Widget>[
+              Image.network('https://msp.c.yimg.jp/images/v2/FUTi93tXq405grZVGgDqGxtNjnmUEa9fGnRT9jceU1GLDjz7uf_VrX6drCtB41Y1NmudLLlLzwcAzNwTUaWnU-vFeodE6Dkr0De3mFD7v_YggfheT3VZvQQXUlSp7XonV8ZzZdl0B_1jHxe-5SA7WATPRVy9yBzLzKuMrDKcmdC3KKF07RK6ISX8jRCtQmoGdFbeGQ38QF-YvSiruEgeJQ==/middle_1284395390.jpg?errorImage=false'),
+              Text(
+                '店の名前', style: TextStyle(fontSize: 15.0, color: HexColor('#31606D')),),
+              new TextField(
+
+              ),
+              Text('待ち時間', style: TextStyle(fontSize: 15.0, color: HexColor('#31606D')),),
+              new TextField(
+
+              ),
+              Text('場所', style: TextStyle(fontSize: 15.0, color: HexColor('#31606D')),),
+              new TextField(
+
+              ),
+              Text('コメント', style: TextStyle(fontSize: 15.0, color: HexColor('#31606D')),),
+              new TextField(
+
+              ),
+              new RaisedButton(
+                child: Text('追加'),
+                onPressed: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyApp(),
+                    ),
+                  );
+                },
+              ),
+            ],
+          )
+
         )
     );
     throw UnimplementedError();
