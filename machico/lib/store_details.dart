215c444c11bcac56cb6c9d8c9d3d711028f250f9
yukino <c1051029@planet.kanazawa-it.ac.jp>
2021-01-22 17:24:14 +0900

    詳細画面の画像、店名、待ち時間などを作った

--- machico/lib/store_details.dart
+++ machico/lib/store_details.dart
@@ -3,20 +3,94 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:machico/hex_color.dart';
 
-class StoreDetails extends StatelessWidget{
+class StoreDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: HexColor('#31606D'),
-      ),
+        appBar: AppBar(
+          backgroundColor: HexColor('#31606D'),
+        ),
         body: Container(
-          margin: EdgeInsets.all(10.0),
+          margin: const EdgeInsets.all(16.0),
           color: HexColor('#F6FFF7'), // HexColorで指定
-          height: double.infinity,
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.network(
+                    'https://mrs.living.jp/wp-content/uploads/2019/08/190802_BOTANIST_eye.jpg'),
+                _shopnameArea(),
+                _messageArea(),
+              ]
+
+          ),
         )
     );
     throw UnimplementedError();
   }
-
 }
+
+Widget _shopnameArea() {
+  return Container(
+      margin: EdgeInsets.all(16.0),
+      child: Row(    // 1行目
+        children: <Widget>[
+          Expanded(  // 2.1列目
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(  // 3.1.1行目
+                  margin: const EdgeInsets.only(bottom: 4.0),
+                  child: Text(
+                    "タピリーノ",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 16.0),
+                  ),
+                ),
+                Container(  // 3.1.2行目
+                  child: Text(
+                    "モチモチおいしいタピオカドリンク",
+                    style: TextStyle(fontSize: 12.0, color: Colors.grey),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Text(
+             '待ち時間',
+             style: TextStyle(
+                 fontWeight: FontWeight.bold,fontSize: 16.0),
+           ),
+
+          Text('30分'),  // 2.3列目
+        ],
+      ),
+  );
+}
+
+Widget _messageArea() {
+  return Container(
+    margin: EdgeInsets.all(16.0),
+    child: Row(    // 1行目
+        children: <Widget>[
+    Expanded(  // 2.1列目
+    child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+    Container(  // 3.1.1行目
+    margin: const EdgeInsets.only(bottom: 4.0),
+    child: Text(
+      "美味しいタピオカモチモチモチモッチャモチャモチャタピオカちゃちゃちゃ",
+      style: TextStyle(
+          fontWeight: FontWeight.bold, fontSize: 16.0),
+    ),
+  ),
+  ],
+  ),
+  ),
+  ],
+  ),
+  );
+}
+
+
+
