abb592b78e6ccd818dbcb53573aa0873992a79e6
KanaiYuta <b1947571@planet.kanazawa-it.ac.jp>
2021-01-22 18:41:00 +0900

    画像の大きさやレイアウトの変更

--- machico/lib/store_addition.dart
+++ machico/lib/store_addition.dart
@@ -12,46 +12,131 @@ class StoreAddition extends StatelessWidget{
           backgroundColor: HexColor('#31606D'),
         ),
         body: Container(
-          margin: EdgeInsets.all(10.0),
-          color: HexColor('#F6FFF7'), // HexColorで指定
-          height: double.infinity,
-          child: Column(
-            children: <Widget>[
-              Image.network('https://msp.c.yimg.jp/images/v2/FUTi93tXq405grZVGgDqGxtNjnmUEa9fGnRT9jceU1GLDjz7uf_VrX6drCtB41Y1NmudLLlLzwcAzNwTUaWnU-vFeodE6Dkr0De3mFD7v_YggfheT3VZvQQXUlSp7XonV8ZzZdl0B_1jHxe-5SA7WATPRVy9yBzLzKuMrDKcmdC3KKF07RK6ISX8jRCtQmoGdFbeGQ38QF-YvSiruEgeJQ==/middle_1284395390.jpg?errorImage=false'),
-              Text(
-                '店の名前', style: TextStyle(fontSize: 15.0, color: HexColor('#31606D')),),
-              new TextField(
-
-              ),
-              Text('待ち時間', style: TextStyle(fontSize: 15.0, color: HexColor('#31606D')),),
-              new TextField(
-
-              ),
-              Text('場所', style: TextStyle(fontSize: 15.0, color: HexColor('#31606D')),),
-              new TextField(
-
-              ),
-              Text('コメント', style: TextStyle(fontSize: 15.0, color: HexColor('#31606D')),),
-              new TextField(
-
-              ),
-              new RaisedButton(
-                child: Text('追加'),
-                onPressed: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => MyApp(),
-                    ),
-                  );
-                },
-              ),
-            ],
-          )
+//            margin: EdgeInsets.all(10.0),
+            color: HexColor('#F6FFF7'),
+            height: double.infinity,
+            child: Column(
+              children: <Widget>[
+                Image.network('https://mrs.living.jp/wp-content/uploads/2019/08/190802_BOTANIST_eye.jpg', height: 250.0, fit: BoxFit.contain,),
+                _storeNameArea(),
+                _waitTimeArea(),
+                _placeArea(),
+                _comentArea(),
+                new RaisedButton(
+                  child: Text('追加'),
+                  onPressed: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MyApp(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            )
 
         )
     );
     throw UnimplementedError();
   }
 
+}
+
+Widget _storeNameArea() {
+  return Container(
+      margin: EdgeInsets.all(8.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Text('店の名前', style: TextStyle(fontSize: 15.0, color: HexColor('#31606D')),),
+                ),
+                Container(
+                  child: new TextField(),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
+  );
+}
+
+Widget _waitTimeArea() {
+  return Container(
+      margin: EdgeInsets.all(8.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Text('待ち時間', style: TextStyle(fontSize: 15.0, color: HexColor('#31606D')),),
+                ),
+                Container(
+                  child: new TextField(
+
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
+  );
+}
+
+Widget _placeArea() {
+  return Container(
+      margin: EdgeInsets.all(8.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Text('場所', style: TextStyle(fontSize: 15.0, color: HexColor('#31606D')),),
+                ),
+                Container(
+                  child: new TextField(
+
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
+  );
+}
+
+Widget _comentArea() {
+  return Container(
+      margin: EdgeInsets.all(8.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Text('コメント', style: TextStyle(fontSize: 15.0, color: HexColor('#31606D')),),
+                ),
+                Container(
+                  child: new TextField(
+
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
+  );
 }
